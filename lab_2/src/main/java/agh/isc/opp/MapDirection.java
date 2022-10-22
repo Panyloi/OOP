9ad263b458783185b8c12205b6a9b71096aeda83
Panyloi <panyloi@git.com>
2022-10-22 21:38:58 +0200

    lab 2

---
+++ lab_2/src/main/java/agh/isc/opp/MapDirection.java
@@ -0,0 +1,51 @@
+package agh.isc.opp;
+
+import java.util.Map;
+
+public enum MapDirection {
+    NORTH,
+    SOUTH,
+    WEST,
+    EAST,
+    ERROR;
+
+    public String toString(){
+        switch (this){
+            case EAST: return "wschód";
+            case WEST: return "zachód";
+            case NORTH: return "północ";
+            case SOUTH: return "połódnie";
+            default: return "Kierunek nie znany!";
+        }
+    }
+
+    public MapDirection next(){
+        switch (this){
+            case NORTH: return EAST;
+            case EAST: return SOUTH;
+            case SOUTH: return WEST;
+            case WEST: return NORTH;
+            default: return ERROR;
+        }
+    }
+
+    public MapDirection previous(){
+        switch (this){
+            case NORTH: return WEST;
+            case WEST: return SOUTH;
+            case SOUTH: return EAST;
+            case EAST: return NORTH;
+            default: return ERROR;
+        }
+    }
+
+    public Vector2d toUnitVector(){
+        switch (this){
+            case NORTH: return new Vector2d(0, 1);
+            case EAST: return new Vector2d(1, 0);
+            case SOUTH: return new Vector2d(0, -1);
+            case WEST: return new Vector2d(-1, 0);
+            default: return new Vector2d(0,0);
+        }
+    }
+}
---
+++ lab_2/src/main/java/agh/isc/opp/MoveDirection.java
@@ -0,0 +1,8 @@
+package agh.isc.opp;
+
+public enum MoveDirection {
+    FORWARD,
+    BACKWARD,
+    RIGHT,
+    LEFT
+}
